8f40076c1d915e0fe0efce763a9ea275e69ad840
rotherda <70058769+rotherda@users.noreply.github.com>
2021-10-01 14:24:13 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# easyinv
+EasyInvestigator Tool
+<h3><i>
+ -python3 install.py
+  <br> -python3 hackingtools.py </br>
+ </h3></i>
---
+++ install.py
@@ -0,0 +1,15 @@
+import os
+os.system("pip install subprocess.run")
+os.system("pip install requests")
+os.system("pip install colorama")
+os.system("pip install urllib3")
+os.system("pip install hashlib")
+os.system("sudo apt-get install figlet")
+os.system("sudo apt-get install urlcrazy")
+os.system("sudo apt-get install dmitry")
+os.system("sudo apt-get install uniscan")
+os.system("sudo apt-get install urlcrazy")
+os.system("sudo apt-get install whatweb")
+print("__________________________________________")
+
+
