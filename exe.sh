2fdaa77fc757570f99dbd0538350919c3178f459
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-10-01 15:07:20 +0300

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -3,5 +3,3 @@ wget https://raw.githubusercontent.com/rotherda/Lucky-Luke-v2.0/main/luckyluke
 chmod 777 luckyluke
 cd /usr/share/
 git clone https://github.com/rotherda/Lucky-Luke-v2.0/
-cd Lucky-Luke-v2.0
-mv luckyluke.py ../
